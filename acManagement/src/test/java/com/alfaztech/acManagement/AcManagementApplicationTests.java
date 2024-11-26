117a173406a55ffc3e493233dc771d106519a284
alfazraja1 <arazaaansari@gmail.com>
2024-11-26 14:48:23 +0530

    added

--- acManagement/src/test/java/com/alfaztech/acManagement/AcManagementApplicationTests.java
+++ acManagement/src/test/java/com/alfaztech/acManagement/AcManagementApplicationTests.java
@@ -3,7 +3,7 @@ package com.alfaztech.acManagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AcManagementApplicationTests {
 
 	@Test
